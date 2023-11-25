fd5656b1fb245efb8849bc45a233fc20b3a4cc03
Sougoumay <elidjaihamid@gmail.com>
2023-11-25 12:32:10 +0100

    initialisation of backend with spring boot and the frontend with angular

---
+++ spring-boot-ecommerce/src/main/java/com/sougoumay/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sougoumay.ecommerce.dao;
+
+import com.sougoumay.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
