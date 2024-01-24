4a613938fd3b11f9e326124bc243a485553546b5
Sougoumay <elidjaihamid@gmail.com>
2024-01-24 14:42:27 +0100

    mise à jour de la quantité des produits lors de l'achat

--- spring-boot-ecommerce/src/main/java/com/sougoumay/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-ecommerce/src/main/java/com/sougoumay/ecommerce/service/CheckoutServiceImpl.java
@@ -1,15 +1,19 @@
 package com.sougoumay.ecommerce.service;
 
 import com.sougoumay.ecommerce.dao.CustomerRepository;
+import com.sougoumay.ecommerce.dao.ProductRepository;
 import com.sougoumay.ecommerce.dao.UserRepository;
 import com.sougoumay.ecommerce.dto.Purchase;
 import com.sougoumay.ecommerce.dto.PurchaseResponse;
 import com.sougoumay.ecommerce.entity.Customer;
 import com.sougoumay.ecommerce.entity.Order;
 import com.sougoumay.ecommerce.entity.OrderItem;
+import com.sougoumay.ecommerce.entity.Product;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -17,6 +21,9 @@ import java.util.UUID;
 public class CheckoutServiceImpl implements CheckoutService{
 
     private CustomerRepository customerRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     public CheckoutServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
@@ -60,6 +67,16 @@ public class CheckoutServiceImpl implements CheckoutService{
 
         // save to the database
         customerRepository.save(customer);
+
+        // update product quantity
+        orderItems.forEach(item -> {
+            Optional<Product> optionalProduct = productRepository.findById(item.getProductId());
+            if (optionalProduct.isPresent()) {
+                Product product = optionalProduct.get();
+                product.setUnitsInStock(product.getUnitsInStock()- item.getQuantity());
+                productRepository.save(product);
+            }
+        });
 
         return new PurchaseResponse(orderTrackingNumber);
     }
